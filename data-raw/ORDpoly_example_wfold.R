316aaba093f4491f3a56a6eb478b2a498c7cb056
Christopher Pollock <chrpol@ceh.ac.uk>
2025-02-25 12:16:44 +0000

    for density decay at whinnyfold

--- data-raw/ORDpoly_example_wfold.R
+++ data-raw/ORDpoly_example_wfold.R
@@ -1,3 +1,5 @@
 ## code to prepare `ORDpoly_example_wfold` dataset goes here
 
+ORDpoly_example_wfold <- sf::read_sf("C:/Users/chrpol/Documents/density_decay_data/owfpolys.shp")
+
 usethis::use_data(ORDpoly_example_wfold, overwrite = TRUE)

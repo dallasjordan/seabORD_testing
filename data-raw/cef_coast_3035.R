5c321d237a3cb822b0ab78a4d836799b2445f844
Christopher Pollock <chrpol@ceh.ac.uk>
2025-02-25 12:47:03 +0000

    corrections to file path

--- data-raw/cef_coast_3035.R
+++ data-raw/cef_coast_3035.R
@@ -1,5 +1,6 @@
 ## code to prepare `cef_coast_3035` dataset goes here
 
-cef_coast_3035 <- readRDS("C:/Users/chrpol/Documents/density-decay-data/cef_coast_3035.rds")
+
+cef_coast_3035 <- readRDS("C:/Users/chrpol/Documents/density_decay_data/cef_coast_3035.rds")
 
 usethis::use_data(cef_coast_3035, overwrite = TRUE)
